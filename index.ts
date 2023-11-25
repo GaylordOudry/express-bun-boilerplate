8376e4a5d4f7c136d69ac773caa86aaaee36ec2e
Gaylord Oudry <gaylord.oudry@eemi.com>
2023-11-25 15:47:47 +0100

    Add basic route configuration

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 import express from "express";
 import cors from "cors";
+import apiRoutes from "./routes/api.routes.ts";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(
-  process.env.PORT || 8000,
-  () =>
+app.use("/api/v1", apiRoutes);
+
+app.listen(process.env.PORT || 8000, () =>
+  console.log(
     `App listening on port ${process.env.PORT || 8000} at http://localhost:${
       process.env.PORT || 8000
     }`,
+  ),
 );
