9a4d58bff8702990f058e3a37c50fe79bf829d06
Gaylord Oudry <gaylord.oudry@eemi.com>
2023-11-25 15:35:20 +0100

    Add and initialize prettier with basic configuration

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(
+  process.env.PORT || 8000,
+  () =>
+    `App listening on port ${process.env.PORT || 8000} at http://localhost:${
+      process.env.PORT || 8000
+    }`,
+);
